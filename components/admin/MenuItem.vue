8954377dafa724a8b63ffb81f6eb9528617db855
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2024-02-15 09:33:43 +0700

    add dependency

---
+++ components/admin/MenuItem.vue
@@ -0,0 +1,40 @@
+<!-- File: MenuItem.vue -->
+
+<template>
+  <div class="px-4">
+    <div
+      :class="
+        route.fullPath === navigate ? 'bg-[#E4262C] fill-white text-white' : ''
+      "
+      @click="router.push(navigate)"
+      class="flex items-center justify-center md:justify-normal cursor-pointer transition-colors duration-200 ease-in-out hover:bg-[#E4262C] rounded-lg px-4 fill-[#888] hover:fill-white hover:text-white text-[#888988] py-3 space-x-2 w-full"
+    >
+      <component :is="iconComponent" class="" />
+      <p class="md:block hidden text-lg">{{ menuText }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  menuText: String,
+  icon: String,
+  navigate: String,
+});
+
+import BsBoxSeam from "~/assets/icon/BsBoxSeam.svg?component";
+import Home from "~/assets/icon/home-outline.svg?component";
+import Student from "~/assets/icon/ph_student.svg?component";
+import Users from "~/assets/icon/users-group-outline.svg?component";
+
+const iconComponents = {
+  BsBoxSeam,
+  Home,
+  Users,
+  Student,
+};
+
+const iconComponent = iconComponents[props.icon];
+const route = useRoute();
+const router = useRouter();
+</script>
--- components/admin/SideNav.vue
+++ components/admin/SideNav.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="bg-white w-[18%] h-screen py-10 rounded-r-xl z-10 relative top-0">
-    <div class="text-center font-bold text-xl">Logo</div>
+  <div
+    class="bg-white w-[18%] h-screen pt-20 rounded-r-xl z-10 relative top-0 flex flex-col"
+  >
+    <div class="flex justify-center font-bold text-xl mb-7">Logo</div>
+    <div class="flex flex-col space-y-4 flex-grow">
+      <MenuItem menuText="Beranda" icon="Home" navigate="/admin/dashboard" />
+      <MenuItem
+        menuText="Guru & Karyawan"
+        icon="Users"
+        navigate="/admin/karyawan"
+      />
+      <MenuItem menuText="Siswa" icon="Student" navigate="/admin/siswa" />
+      <MenuItem
+        menuText="Layanan Kirim"
+        icon="BsBoxSeam"
+        navigate="/admin/layanan"
+      />
+    </div>
+    <div class="bg-gray-100 h-20 flex px-4 items-center">
+      <div class="flex items-center w-full space-x-2 justify-normal">
+        <NuxtImg
+          class="w-12 h-12 rounded-full self-center"
+          src="/dashboard/test_image.png"
+          alt="Rounded avatar"
+        />
+        <div class="flex flex-col items-start justify-center">
+          <h4 class="text-sm">Atsal Faiz</h4>
+          <p class="text-sm">Atsalfaiz@gmail.com</p>
+        </div>
+        <div class="cursor-pointer flex pl-4"><LogoutIcon /></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MenuItem from "./MenuItem.vue";
+import LogoutIcon from "../../assets/icon/logout.svg?component";
+</script>
