e95a6785d524d325fe24a4d70bb2a840370bea1b
Atsal <atsalfaizpramatya@gmail.com>
2024-02-15 09:35:44 +0700

    wellcoming forget and login

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -7,18 +7,17 @@
                 <div
                     class="grid flex-wrap items-center justify-center grid-cols-1 mx-auto shadow-xl lg:grid-cols-1 rounded-[40px]">
                     <div class="w-full px-8 py-5">
-                        <div>
-                            <div class="flex justify-center w-full mt-4">
-                                <img class="object-cover h-full bg-cover rounded-l-lg" width="50"
-                                    src="public/assets/telkom.png" alt="" />
-                            </div>
-                            <div class="flex my-8 sm:mt-5">
-                                <div class="inline-flext flex-col items-center justify-center w-full">
-                                    <h3 class="text-sm font-semibold text-black leading-6 lg:text-xl text-center">Buku Tamu
-                                    </h3>
-                                    <p class="text-sm font-semibold text-black leading-6 lg:text-xl text-center">SMK Telkom
-                                        Malang</p>
-                                </div>
+                        <div class="flex justify-center w-full mt-4">
+                            <img class="object-cover h-full bg-cover rounded-l-lg" width="50" src="/assets/telkom.png"/>
+                        </div>
+
+
+                        <div class="flex my-8 sm:mt-5">
+                            <div class="inline-flext flex-col items-center justify-center w-full">
+                                <h3 class="text-sm font-semibold text-black leading-6 lg:text-xl text-center">Buku Tamu
+                                </h3>
+                                <p class="text-sm font-semibold text-black leading-6 lg:text-xl text-center">SMK Telkom
+                                    Malang</p>
                             </div>
                         </div>
 
@@ -46,15 +45,23 @@
                                         </button>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="flex flex-col mt-4 lg:space-y-2 py-6">
-                                    <button type="submit" name="login"
-                                        class="flex items-center text-base justify-center mx-14 py-2 font-medium text-center text-white transition duration-500 ease-in-out transform bg-[#E4262C] rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 lg:w-auto">
-                                        Login
-                                    </button>
-                                </div>
+                            <div class="flex flex-col mt-4 lg:space-y-2 py-6">
+                                <button type="submit" name="login"
+                                    class="flex items-center text-base justify-center mx-20 py-2 font-medium text-center text-white transition duration-500 ease-in-out transform bg-[#E4262C] rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 lg:w-auto">
+                                    Login
+                                </button>
                             </div>
                         </form>
+
+
+                        <div class="text-sm text-center hover:text-red-500 my-2">
+                            <button @click="navigateTo('/admin/forget_password')">
+                                <h3>Lupa Kata Sandi</h3>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -66,9 +73,9 @@
 import axios from "../plugins/axios";
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { useAuth } from "~/stores/general";
-import { defineStore } from "pinia";
+import { useGeneralStore } from "~/stores/general"; // Importing the Pinia store
 import Swal from 'sweetalert2';
+
 
 export default {
     setup() {
@@ -76,79 +83,112 @@ export default {
         const password = ref('');
         const showPassword = ref(false);
         const router = useRouter();
+        const generalStore = useGeneralStore(); // Accessing the Pinia store
 
-        const auth_data = computed(() => {
-            useAuth().isAuth_data = true
-        })
+        // Variabel reaktif untuk mengontrol tampilan form lupa kata sandi
+        const forgotPassword = ref(false);
+        const newPassword = ref('');
+        const confirmPassword = ref('');
 
         const togglePasswordVisibility = () => {
             showPassword.value = !showPassword.value;
         };
 
-        const handleLogin = () => {
+        const toggleForgotPassword = () => {
+            forgotPassword.value = !forgotPassword.value;
+        };
+
+        const handleLogin = async () => {
             const loginData = {
                 email: email.value,
                 password: password.value,
             };
 
+            try {
+                const response = await axios.post('/login/', loginData);
+                console.log('Login successful:', response.data);
+                localStorage.setItem('token', response.data.token);
 
-            const defaultUrl = process.env.API_BASE;
+                // Update the store after successful login
+                generalStore.Hello(response.data.user_name); // Assuming you receive user_name in the response
+                await generalStore.getApiPokemon('some_type'); // Example usage of another action
 
-            const api = axios.create({
-                baseURL: defaultUrl,
-                // headers: {
-                //   common: {
-                //     Authorization: Bearer ${localStorage.getItem("token")},
-                //   },
-                // },
-            });
+                // Navigate to the dashboard page after successful login
+                router.push('/dashboard');
 
-            api.post('/login/', loginData)
-                .then((response) => {
-                    console.log('Login successful:', response.data);
-                    localStorage.setItem('token', response.data.token)
-                    // Navigasi ke halaman dashboard setelah login berhasil
-
-                    router.push('/dashboard');
-
-                    Swal.fire({
-                        icon: 'success',
-                        title: '<span style="font-size: 40px; font-weight: bold; color: green;"> Welcome Back! </span>',
-                        showConfirmButton: false,
-                        timer: 2000, // Auto-close the alert after 2 seconds
-                        didClose: () => {
-                            // Optionally perform any action after the alert is closed
-                        },
-                    });
-                })
-                .catch((error) => {
-                    if (error.response) {
-                        // Objek error.response ada, kita dapat membaca propertinya
-                        console.error('Login failed. Server response:', error.response);
-                        console.error('Error message:', error.response.data);
-                    } else {
-                        // Objek error.response tidak ada, mungkin masalah koneksi atau server mati
-                        console.error('Login failed. Unable to connect to the server.');
-                    }
-                    Swal.fire({
-                        icon: 'error',
-                        title: '<span style="font-size: 40px; font-weight: bold; color: red; padding-top: 5px;"> Login Failed! </span>',
-                        html: '<div style="padding-bottom: 10px; font-weight: bold;">Invalid email or password. Please try again</div>',
-                        confirmButtonColor: '#d33',
-                        confirmButtonText: 'Try Again!',
-                    });
+                Swal.fire({
+                    icon: 'success',
+                    title: '<span style="font-size: 40px; font-weight: bold; color: green;"> Welcome Back! </span>',
+                    showConfirmButton: false,
+                    timer: 2000,
+                    didClose: () => {
+                        // Optionally perform any action after the alert is closed
+                    },
                 });
+            } catch (error) {
+                if (error.response) {
+                    console.error('Login failed. Server response:', error.response);
+                    console.error('Error message:', error.response.data);
+                } else {
+                    console.error('Login failed. Unable to connect to the server.');
+                }
+                Swal.fire({
+                    icon: 'error',
+                    title: '<span style="font-size: 40px; font-weight: bold; color: red; padding-top: 5px;"> Login Failed! </span>',
+                    html: '<div style="padding-bottom: 10px; font-weight: bold;">Invalid email or password. Please try again</div>',
+                    confirmButtonColor: '#d33',
+                    confirmButtonText: 'Try Again!',
+                });
+            }
+        };
+
+        const handleResetPassword = async () => {
+            // Logika untuk penanganan reset password
         };
 
         return {
             email,
             password,
             showPassword,
+            forgotPassword,
             togglePasswordVisibility,
             handleLogin,
+            toggleForgotPassword,
         };
     },
 };
 </script>
   
-<style scoped>@import '@fortawesome/fontawesome-free/css/all.css';</style>
+<style scoped>
+@import '@fortawesome/fontawesome-free/css/all.css';
+</style>
+
+
+
+
+
+<!-- <form @submit.prevent="handleResetPassword">
+    <div class="mt-6 space-y-6 mx-6">
+        <div>
+            <h3 class="mb-1">Password Baru</h3>
+            <label for="newPassword" class="sr-only">Password Baru</label>
+            <input required type="password" name="newPassword" id="newPassword"
+                class="block w-full px-3 py-1 text-base bg-gray-50 placeholder-gray-300 transition duration-500 ease-in-out transform border-2 border-gray-300 rounded-lg bg-with-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                placeholder="Masukkan Password Baru" v-model="newPassword" />
+        </div>
+        <div>
+            <h3 class="mb-1">Konfirmasi Password Baru</h3>
+            <label for="confirmPassword" class="sr-only">Konfirmasi Password Baru</label>
+            <input required type="password" name="confirmPassword" id="confirmPassword"
+                class="block w-full px-3 py-1 text-base bg-gray-50 placeholder-gray-300 transition duration-500 ease-in-out transform border-2 border-gray-300 rounded-lg bg-with-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                placeholder="Konfirmasi Password Baru" v-model="confirmPassword" />
+        </div>
+    </div>
+
+    <div class="flex flex-col mt-4 lg:space-y-2 py-6">
+        <button type="submit" name="resetPassword"
+            class="flex items-center text-base justify-center mx-14 py-2 font-medium text-center text-white transition duration-500 ease-in-out transform bg-[#E4262C] rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 lg:w-auto">
+            Reset Password
+        </button>
+    </div>
+</form> -->
---
+++ pages/user/wellcoming.vue
@@ -0,0 +1,50 @@
+<template>
+    <section>
+        <div class="h-full w-full bg-[#F6F7FF]">
+            <img class="" src="/wellcompage/wp.svg" alt="">
+
+            <div class="flex justify-center gap-48 py-20">
+                <div class="my-36">
+                    <h1 class="font-semibold text-5xl">Bertamu dan kirim paket <br>
+                        lebih mudah dengan <span style="color:red"> buku <br> tamu!</span>
+                    </h1>
+                    <hr class="border-t-4 border-[#E4262C] my-4 w-96">
+                    <h3 class="text-lg">
+                        Proses digitalisasi penerimaan tamu dan kirim paket dengan <br> Buku Tamu!
+                    </h3>
+                    <button class="text-white py-2 px-6 my-4 rounded-lg bg-[#E4262C]">
+                        Buat Janji
+                    </button>
+                </div>
+                <div class="my-24">
+                    <img src="/wellcompage/wp2.svg">
+                </div>
+            </div>
+
+            <div class="">
+                <h1 class="font-semibold text-3xl flex justify-center my-10">Daftar Pilihan</h1>
+
+                <div class="flex justify-center gap-20 mx-40 pb-40">
+                    <div class="flex flex-col justify-center w-full h-full bg-white rounded-xl border-t-8 border-t-[#E4262C] ">
+                        <img src="/wellcompage/siswa.svg" class="py-14 px-16">
+                        <h1 class="flex justify-center font-semibold text-2xl  py-2">Siswa</h1>
+                    </div>
+
+                    <div class="flex flex-col justify-center w-full h-full bg-white rounded-xl border-t-4 border-t-[#E4262C] ">
+                        <img src="/wellcompage/guru.svg" class="py-20 px-24">
+                        <h1 class="flex justify-center font-semibold text-2xl">Guru/Kurir</h1>
+                    </div>
+
+                    <div class="flex flex-col justify-center w-full h-full bg-white rounded-xl border-t-4 border-t-[#E4262C] ">
+                        <img src="/wellcompage/kurir.svg" class="py-20 px-10">
+                        <h1 class="flex justify-center font-semibold text-2xl py-4">layanan Kirim</h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
